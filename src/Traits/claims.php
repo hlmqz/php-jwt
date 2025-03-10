46e840306aa15ddb15a2177499d6b1e119a8f88f
H Marquez <hlmarquezm@gmail.com>
2025-03-10 13:38:21 -0500

    Agregadas funcionalidades para claims por nombres

--- src/Traits/claims.php
+++ src/Traits/claims.php
@@ -5,11 +5,32 @@ use Hlmqz\JWT\Exceptions\ClaimInvalidException;
 
 trait claims
 {
+
+	public string $issuer
+	{
+		set(string $value){ $this->setClaim('iss',$value); }
+		get{ return $this->claimsData['iss'] ?? ''; }
+	}
+
+	public string $subject
+	{
+		set(string $value){ $this->setClaim('sub',$value); }
+		get{ return $this->claimsData['sub'] ?? ''; }
+	}
+
+	public string|array $audience
+	{
+		set(string|array $value){ $this->setClaim('aud',$value); }
+		get{ return $this->claimsData['aud'] ?? ''; }
+	}
+
 	protected $claims = [
-		'iss', 'sub', 'aud', 'exp',
+		'iss', // Issuer
+		'sub', // Subject
+		'aud', // Audience
 		/*
 		Claims auto management:
-		iat, jti, nbf
+		iat, jti, nbf, exp
 		*/
 	];
 
@@ -21,9 +42,11 @@ trait claims
 	{
 		$named = mb_strtolower($name);
 		if(!in_array($named, $this->claims))
-			throw new ClaimInvalidException("The name of the claim is non-standard");
+			throw new ClaimInvalidException(
+				"The name of the claim is non-standard: ".implode(', ', $this->claims)
+			);
 
-		if(is_null($value) && isset($this->claimsData[$named]))
+		if( !$value && isset($this->claimsData[$named]))
 			unset($this->claimsData[$named]);
 		else
 			$this->claimsData[$named] = $value;
